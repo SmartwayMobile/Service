90c8bd2dd7d893f023bee420f2db005ba4333b3f
Conner Bush <connerbush@gmail.com>
2017-12-06 22:34:49 -0600

    updates pseudocode

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ const openDataService = require('./openDataService');
 const geometryService = require('./geometryService');
 
 console.log(geometryService.test());
+
+// get users from firebase (optional: where currentTiem between route.startTime and route.endTime)
+var users = [];
+// get incidents from opendata
+var incidents = [];
+incidents = openDataService.getIncidents();
+
+// loop through each user
+users.forEach((user) => {
+  // loop through each route
+  user.routes.forEach((route) => {
+    geometryService.getRouteFromLocations(route.startAddress, route.endAddress).then((polyline) => {
+      incidents.forEach((incident) => {
+        if (geometryService.isPointCloseToLine(incident, polyline)) {
+          notifyUser(user, incident);
+        }
+      });
+    });
+  });
+});
+
+function notifyUser(user, incident) {
+  var msg = `Incident reported on ${incident.route}. Consider alternate route.`;
+  // push msg to user
+}
