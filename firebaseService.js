7a9c1e73572224ca954437f296cee5d50a2974d9
Conner Bush <connerbush@gmail.com>
2017-12-10 13:34:44 -0600

    adds polling of incidents for push notifications

--- firebaseService.js
+++ firebaseService.js
@@ -1,5 +1,5 @@
 var admin = require("firebase-admin");
-
+var fs = require('fs');
 var serviceAccount = require("./smartway-mobile-1512405241266-firebase-adminsdk-7df7n-30c5543ee3.json");
 
 admin.initializeApp({
@@ -22,4 +22,64 @@ exports.getUsers = () => {
     }, reject);
 
   });
+}
+
+exports.getIncidents = () => {
+  let incidents = [];
+  return new Promise((resolve, reject) => {
+    var ref = admin.database().ref("incidents");
+
+    ref.on('value', (snapshot) => {
+      incidents = Object.keys(snapshot.val()).map(k => {
+        const incident = snapshot.val()[k];
+        incident.uuid = k;
+        return incident;
+      });
+      resolve(incidents);
+    }, reject);
+
+  });
+}
+
+exports.sendMessage = (message) => {
+  // This registration token comes from the client FCM SDKs.
+  var registrationToken = "dNEOCU68kuw:APA91bFn-Ktsot_I12qFOw_4732_bTxwCzsnUr26v898fS8fRyUrH5tbLV067x44Wyg-OSB3T-zAzv71cusbr8xTPkYjqia7GhYLwGFxz5awx__1GZjdiq_OdtsmYBVaseVLV2zBBEbM";
+
+  // See the "Defining the message payload" section below for details
+  // on how to define a message payload.
+  var payload = {
+    data: { msg: message }
+  };
+
+  // Send a message to the device corresponding to the provided
+  // registration token.
+  admin.messaging().sendToDevice(registrationToken, payload)
+    .then(function (response) {
+      // See the MessagingDevicesResponse reference documentation for
+      // the contents of response.
+      console.log("Successfully sent message:", response);
+      process.exit(0);
+    })
+    .catch(function (error) {
+      console.log("Error sending message:", error);
+    });
+}
+
+exports.addIncidents = (ids) => {
+
+  // var stream = fs.createWriteStream("my_file.txt");
+  // stream.once('open', function (fd) {
+  //   stream.write("My first row\n");
+  //   stream.write("My second row\n");
+  //   stream.end();
+  // });
+  var ref = admin.database().ref();
+  ref.child('incidents').set({
+    ids
+  });
+  // const path = `/incidents`;
+  // return firebase.push(path, incidents)
+  //   .then(pushResult => {
+  //     console.log('incidents saved.');
+  //   });
 }
--- index.js
+++ index.js
@@ -1,60 +1,57 @@
 const openDataService = require('./openDataService');
 const geometryService = require('./geometryService');
 const firebaseService = require('./firebaseService');
+const _ = require('lodash');
+
+var msgArr = [];
 
 // get users from firebase (optional: where currentTiem between route.startTime and route.endTime)
-openDataService.getIncidents().then(res => {
-  const incidents = res.data;
-  firebaseService.getUsers().then((users) => {
-    users.forEach(user => {
-      const routeKeys = Object.keys(user.routes);
-      routeKeys.forEach(routeKey => {
-        const userRoute = user.routes[routeKey];
-        const routeName = userRoute.name;
-        geometryService.getRouteFromLocations(`${userRoute.startCoords.lat},${userRoute.startCoords.lng}`,
-          `${userRoute.endCoords.lat},${userRoute.endCoords.lng}`).then((obj) => {
-            incidents.forEach(incident => {
-              const incidentLocation = incident.locations[0].coordinates[0];
-              if (!incidentLocation) return;
-              obj.json.routes.forEach(route => {
-                let path = [];
-                route.legs.forEach(leg => {
-                  leg.steps.forEach(step => {
-                    let start = step.start_location;
-                    let end = step.end_location;
-                    path.push([start.lat, start.lng], [end.lat, end.lng]);
+firebaseService.getIncidents().then(notifiedIncidents => {
+  let incidentsToSave = [];
+  incidentsToSave.push(...notifiedIncidents[0]);
+  openDataService.getIncidents().then(res => {
+    const incidents = res.data;
+    firebaseService.getUsers().then((users) => {
+      users.forEach(user => {
+        const routeKeys = Object.keys(user.routes);
+        routeKeys.forEach(routeKey => {
+          const userRoute = user.routes[routeKey];
+          const routeName = userRoute.name;
+          geometryService.getRouteFromLocations(`${userRoute.startCoords.lat},${userRoute.startCoords.lng}`,
+            `${userRoute.endCoords.lat},${userRoute.endCoords.lng}`).then((obj) => {
+              incidents.forEach(incident => {
+                if (notifiedIncidents[0].indexOf(incident.id) > -1) return;
+                incidentsToSave.push(incident.id);
+                const incidentLocation = incident.locations[0].coordinates[0];
+                if (!incidentLocation) return;
+                obj.json.routes.forEach(route => {
+                  let path = [];
+                  route.legs.forEach(leg => {
+                    leg.steps.forEach(step => {
+                      let start = step.start_location;
+                      let end = step.end_location;
+                      path.push([start.lat, start.lng], [end.lat, end.lng]);
+                    });
                   });
+                  if (geometryService.isPointCloseToLine([incidentLocation.lat, incidentLocation.lng], path, 1)) {
+                    notifyUser(user, route.summary);
+                  }
                 });
-                incident = [36.1728045, -86.7652469];
-                if (geometryService.isPointCloseToLine([incidentLocation.lat, incidentLocation.lng], path, 0.05)) {
-                  //notifyUser(user, incident);
-                  console.log(`Incident along your route ${routeName} at ${route.summary}. Consider alternate route.`);
-                }
+                firebaseService.addIncidents(_.uniq(incidentsToSave));
               });
-            });
-          }).catch(err => console.log(err));
+
+            }).catch(err => { console.log(err); process.exit(0); });
+        });
       });
-    });
-  }).catch(err => console.log(err));
-}).catch(err => console.log(err));
-// get incidents from opendata
-// incidents = openDataService.getIncidents();
+    }).catch(err => { console.log(err); process.exit(0); });
 
-// loop through each user
-// users.forEach((user) => {
-//   // loop through each route
-//   user.routes.forEach((route) => {
-//     geometryService.getRouteFromLocations(route.startAddress, route.endAddress).then((polyline) => {
-//       incidents.forEach((incident) => {
-//         if (geometryService.isPointCloseToLine(incident, polyline)) {
-//           notifyUser(user, incident);
-//         }
-//       });
-//     });
-//   });
-// });
+  }).catch(err => { console.log(err); process.exit(0); });
+}).catch(err => { console.log(err); process.exit(0); });
 
-function notifyUser(user, incident) {
-  var msg = `Incident reported on ${incident.route}. Consider alternate route.`;
-  // push msg to user
+function notifyUser(user, route) {
+  var msg = `Incident reported on ${route}. Consider alternate route.`;
+  console.log(msg);
+  if (msgArr.indexOf(msg) > -1) return;
+  firebaseService.sendMessage(msg);
+  msgArr.push(msg);
 }
